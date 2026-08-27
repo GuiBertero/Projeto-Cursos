5b4a6933d3a541fc9ff62c916032b93d4d793a3b
GuiBertero <guibcoliveira@gmail.com>
2026-08-27 00:40:52 -0300

    Create Disciplina.cs

---
+++ Disciplina.cs
@@ -0,0 +1,108 @@
+using System;
+
+namespace ProjetoCursos
+{
+    public class Disciplina
+    {
+        private int id;
+        private string descricao;
+        private Aluno[] alunos;
+
+        public Disciplina(int id, string descricao)
+        {
+            this.id = id;
+            this.descricao = descricao;
+            this.alunos = new Aluno[15];
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+        }
+
+        public bool matricularAluno(Aluno aluno)
+        {
+            // Verifica se já está matriculado
+            for (int i = 0; i < 15; i++)
+            {
+                if (alunos[i] != null &&
+                    alunos[i].Id == aluno.Id)
+                {
+                    return false;
+                }
+            }
+
+            // Procura uma posição vazia
+            for (int i = 0; i < 15; i++)
+            {
+                if (alunos[i] == null)
+                {
+                    alunos[i] = aluno;
+                    return true;
+                }
+            }
+
+            // Disciplina cheia
+            return false;
+        }
+
+        public bool desmatricularAluno(Aluno aluno)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (alunos[i] != null &&
+                    alunos[i].Id == aluno.Id)
+                {
+                    alunos[i] = null;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool alunoMatriculado(Aluno aluno)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (alunos[i] != null &&
+                    alunos[i].Id == aluno.Id)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int quantidadeAlunos()
+        {
+            int quantidade = 0;
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (alunos[i] != null)
+                {
+                    quantidade++;
+                }
+            }
+
+            return quantidade;
+        }
+
+        public Aluno getAluno(int posicao)
+        {
+            if (posicao >= 0 && posicao < 15)
+            {
+                return alunos[posicao];
+            }
+
+            return null;
+        }
+    }
+}
